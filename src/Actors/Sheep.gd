f6b06043da044fbd14e1cbfd3856939dc4a6b3b7
Jelle Nauta <jelle.nauta@tno.nl>
2020-04-19 20:01:25 +0200

    Fixed sheep hopefully for good

--- src/Actors/Sheep.gd
+++ src/Actors/Sheep.gd
@@ -11,25 +11,29 @@ var eat_rate = 30
 var eat_countdown = 0
 var eat_duration = 1
 var move_dir
+var sight = 4
+var objects
 
 func _ready():
 	connect("update_stomach", $"/root/Game/UI/Stomach", "update_stomach")
 	set_coords(Vector2(0, 0))
-	set_move_dir(Vector2(1, 0))
-	move_dir = Vector2(1, 0)
+	set_move_dir(Vector2(0, 0))
+	objects = $"/root/Game/Objects"
 
 func set_move_dir(move_vector_diam):
 	move_dir = move_vector_diam
 	var move_vector_rect = Globals.diam_to_rect(move_vector_diam)
-#	print(move_vector_rect)
-	if move_vector_rect.x  >= 0:
-		$AnimationPlayer/sprite.flip_h = false
+	if move_vector_rect == Vector2(0, 0):
+		$AnimationPlayer/sprite.animation = "idle"
 	else:
-		$AnimationPlayer/sprite.flip_h = true
-	if move_vector_rect.y >= 0:
-		$AnimationPlayer/sprite.animation = "jump_se"
-	else:
-		$AnimationPlayer/sprite.animation = "jump_ne"
+		if move_vector_rect.y >= 0:
+			$AnimationPlayer/sprite.animation = "jump_se"
+		elif move_vector_rect.y < 0:
+			$AnimationPlayer/sprite.animation = "jump_ne"
+		if move_vector_rect.x  >= 0:
+			$AnimationPlayer/sprite.flip_h = false
+		elif move_vector_rect.x < 0:
+			$AnimationPlayer/sprite.flip_h = true
 
 func set_coords(coords, level = 0):
 	.set_coords(coords)
@@ -40,14 +44,31 @@ func jump_ends():
 	if Globals.game_over:
 		return
 	self.set_coords(self.diam_coords + self.move_dir)
-	# Eat the flower
-	$"/root/Game/Objects".eat_at_coords(self.diam_coords, self)
+	# Decide next jump
+	var left_dir = move_dir.rotated(0.5 * PI)
+	var right_dir = move_dir.rotated(-0.5 * PI)
+	var dirs = [move_dir, left_dir, right_dir]
+	var flower_found = false
+	for dist in sight:
+		for dir in dirs:
+			var objs = objects.get_objects(self.diam_coords + dist * dir)
+			if objs != null:
+				for obj in objs:
+					if obj is Flower:
+						set_move_dir(dir)
+						flower_found = true
+						break
+			if flower_found: break
+		if flower_found: break
+	# Eat flowers, if any
+	objects.eat_at_coords(self.diam_coords, self)
 
 func die():
+	print("dying?")
 	destination = null
 	path = null
 	next_path_node = null
-	self.move_dir = Vector2(0, 0)
+	set_move_dir(Vector2(0, 0))
 	Globals.game_over = true
 	get_tree().change_scene("res://src/Screens/EndScreen.tscn")
 
